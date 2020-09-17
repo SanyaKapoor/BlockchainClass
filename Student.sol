026e63c04eee7dd29502035d67e99f5661dcccca
SanyaKapoor <31134079+SanyaKapoor@users.noreply.github.com>
2020-09-17 19:54:05 +0530

    Create Student.sol

---
+++ Student.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.17;
+contract Student {
+    
+    struct stu
+    {
+        string name;
+        int number;
+        string gender;
+        int year;
+    }
+    
+    mapping (address => stu) students;
+    address[] public studentsAccts;
+    
+    function setInstructor(string memory name, string memory gender, int number, int year, address Number ) public {
+        stu memory student = students[Number];
+        student.name = name;
+        student.gender = gender;
+        student.year = year;
+        student.number = number;
+        studentsAccts.push(Number) -1;
+    }
+    
+    function getStudent(address Number) view public returns (string memory, string memory, int, int) {
+        return (students[Number].name, students[Number].gender, students[Number].number, students[Number].year);
+    }
+    
+}
